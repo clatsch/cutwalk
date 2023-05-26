03f12f8bdface3a380b29dd26ee427b3d76f08db
Cla Tschenett <cla@aimara.ch>
2023-05-26 17:22:02 +0200

    a small step for humanity, a big step for me - jwt working

--- controllers/cutOptionsController.js
+++
@@ -1,6 +0,0 @@
-import CutOption from '../models/cutOptionsModel.js';
-import * as factory from './handlerFactory.js';
-
-export const getAllCutOptions = factory.getAll(CutOption);
-export const createCutOption = factory.createOne(CutOption);
-export const updateCutOption = factory.updateOne(CutOption);
