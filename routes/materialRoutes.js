14f88017a71c29708181824665679c9421d3a0f0
Cla Tschenett <cla@aimara.ch>
2023-06-20 19:28:44 +0200

    getAllMaterials only protected, not restricted to admin

--- routes/materialRoutes.js
+++ routes/materialRoutes.js
@@ -6,13 +6,16 @@ const router = express.Router();
 
 // This middleware will protect all the routes that come after this point
 router.use(authController.protect);
+
+router.route('/')
+    .get(materialController.getAllMaterials);
 // This middleware will restrict all the routes that come after this point
 router.use(authController.restrictsTo('admin'));
 
 // Route for handling materials
 router.route('/')
-    .post(materialController.createMaterial)
-    .get(materialController.getAllMaterials);
+    .post(materialController.createMaterial);
+
 
 // Route for updating a material
 router.route('/:id')
